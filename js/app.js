3e05045596641fe69dd208aa99714baf8c4f1f2e
Waelragob <93447892+Waelragob@users.noreply.github.com>
2025-05-22 05:33:00 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -412,3 +412,22 @@
 
 }(jQuery));
 
+
+    document.addEventListener("DOMContentLoaded", function () {
+      var backToTop = document.getElementById("backToTop");
+      window.addEventListener("scroll", function () {
+        if (window.pageYOffset > 300) {
+          backToTop.style.display = "block";
+        } else {
+          backToTop.style.display = "none";
+        }
+      });
+
+      backToTop.addEventListener("click", function () {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth"
+        });
+      });
+    });
+  
